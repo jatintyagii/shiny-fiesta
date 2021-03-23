93382f262defeecac87fe55664e1207a846c4c65
jatin <tyagijatin1234@gmail.com>
2021-03-23 16:36:59 +0530

    add show in that

--- dummyproject/src/go/Hello.java
+++ dummyproject/src/go/Hello.java
@@ -1,7 +1,12 @@
 package go;
 
 public class Hello {
+	public void show() {
+		System.out.println("you are in the show() function");
+	}
 public static void main(String[] args) {
 	System.out.println("hello everyone");
+	Hello c=new Hello();
+	c.show();
 }
 }
