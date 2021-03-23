c322fadd268be126cb91f8b088033e8df45544df
jatin <tyagijatin1234@gmail.com>
2021-03-23 18:03:09 +0530

    new project is made

--- dummyproject/src/go/Sing.java
+++ dummyproject/src/go/Sing.java
@@ -3,6 +3,7 @@ package go;
 public class Sing {
 	public static void main(String[] args) {
 		System.out.println("this is the sing class in java");
+		System.out.println("a new line added");
 	}
 
 }
